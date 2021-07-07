e54f6b48ea6ba71212ba2bf5e6fbdd5c20a1f225
AbubakarKhawaja <abubakarkhawaja@A006-00558.local>
2021-07-07 15:51:03 +0500

    task 5 added

--- weatherman.py
+++ weatherman.py
@@ -1,12 +1,14 @@
 from tasks.task3 import task3
 from tasks.task2 import task2
 from tasks.task1 import task1
-
+from tasks.task5 import task5
 #to take command line argument
 import sys
+
+
 def main():
     arguments = sys.argv
-    # print(arguments)
+
     if "-e" in arguments:
         print('Task 1:')
         task1('/Users/abubakarkhawaja/Documents/weatherfiles/',arguments[arguments.index('-e')+1])
@@ -18,5 +20,9 @@ def main():
     if "-c" in arguments:
         print('Task 3:')
         task3('/Users/abubakarkhawaja/Documents/weatherfiles/',arguments[arguments.index('-c')+1])
+    if "-d" in arguments:
+        print('Task 5:')
+        task5('/Users/abubakarkhawaja/Documents/weatherfiles/',arguments[arguments.index('-d')+1])
+
 if __name__ == "__main__":
     main()
