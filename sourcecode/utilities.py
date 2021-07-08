301eaf613f7a310ebd0ea64adbd66ab013460fe9
AbubakarKhawaja <abubakarkhawaja@A006-00558.local>
2021-07-08 17:53:44 +0500

    cleaned data for file handling

--- sourcecode/utilities.py
+++ sourcecode/utilities.py
@@ -1,30 +1,21 @@
 import os
 import calendar
+import pandas as pd
 
 def get_Weather_Info(path):
+    df = pd.read_csv(path)
+    # cleaning
+    df.columns = df.columns.str.strip()
+    df.dropna(subset = ['Max TemperatureC', 'Mean TemperatureC', 'Min TemperatureC', 
+                            'Max Humidity', 'Mean Humidity', 'Min Humidity'], 
+                            how='any', inplace=True)
+    df = df.set_index('PKT')
+    # filling data in dictionary
     weather_data = {}
-    column = []
-    with open(path, 'r') as file:
-        flag = True
-        for content in file.readlines():
-            # condtion to read column names
-            if flag:
-                column = content.split(',')
-                flag = False
-            else:        
-                # from 1 because dont want to read columns again s
-                for i in range(1, len(content)):
-                    # takinng values of first lines
-                    column_data = content.split(',')
-                    if column_data[1] == "":
-                        continue
-                    # using date as key for dictionary
-                    weather_data[column_data[0]] = {}
-                    for k in range(len(column)):
-                        # saving values in their respective column according to date
-                        key = column[k].strip()
-                        weather_data[column_data[0]][key] = content.split(',')[k].strip()
-        file.flush()
+    weather_data = { column : '' for column in df.columns}
+    for column in df.columns:
+        weather_data[column] = {date : df[column][date] for date in df.index.values}     
+    # print(weather_data)
     return weather_data
 
 def getWeatherFiles(date, path):
@@ -37,4 +28,30 @@ def getWeatherFiles(date, path):
     else: 
         weatherFiles = [dir for dir in directory if date in dir]
     return weatherFiles
-    
+
+# def get_Weather_Info(path):
+    # weather_data = {}
+    # column = []
+    # with open(path, 'r') as file:
+    #     flag = True
+    #     for content in file.readlines():
+    #         # condtion to read column names
+    #         if flag:
+    #             column = content.split(',')
+    #             flag = False
+    #         else:        
+    #             # from 1 because dont want to read columns again s
+    #             for i in range(1, len(content)):
+    #                 # takinng values of first lines
+    #                 column_data = content.split(',')
+    #                 if column_data[1] == "":
+    #                     continue
+    #                 # using date as key for dictionary
+    #                 weather_data[column_data[0]] = {}
+    #                 for k in range(len(column)):
+    #                     # saving values in their respective column according to date
+    #                     key = column[k].strip()
+    #                     weather_data[column_data[0]][key] = content.split(',')[k].strip()
+    #     file.flush()
+    # return weather_data
+              
