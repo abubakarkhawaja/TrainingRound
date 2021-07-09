a4299c1c9fa0a7769a5cba37aff509a84bcada5d
AbubakarKhawaja <abubakarkhawaja@A006-00558.local>
2021-07-09 16:20:54 +0500

    added setup and teardown method in TestUtilities

--- sourcecode/test_utilities.py
+++ sourcecode/test_utilities.py
@@ -3,15 +3,37 @@ from utilities import get_Weather_Info, get_Weather_Files
 
 class TestUtilites(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Will run at start of unittesting this class"""
+        print('setUpClass')
+    
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Will run at end of unittesting this class"""
+        print('tearDownClass')
+
+    def setUp(self):
+        """Will run before every testcase"""
+        print('setUp')
+        self.filePath = '/Users/abubakarkhawaja/Documents/weatherfiles/Murree_weather_2004_Aug.txt'
+        self.directoryPath = '/Users/abubakarkhawaja/Documents/weatherfiles/'
+        
+    def tearDown(self):
+        """Will run after every testcase"""
+        print('tearDown\n')
+
     def test_get_Weather_Info(self):
-        filePath = '/Users/abubakarkhawaja/Documents/weatherfiles/Murree_weather_2004_Aug.txt'
-        self.assertIsInstance(get_Weather_Info(filePath), dict)    
+        self.assertIsInstance(get_Weather_Info(self.filePath), dict)    
+
         self.assertIsNone(get_Weather_Info(''))
+        print('test_get_Weather_Info')
 
     def test_get_Weather_Files(self):
-        directoryPath = '/Users/abubakarkhawaja/Documents/weatherfiles/'
-        self.assertIsInstance(get_Weather_Files('2004/6', directoryPath), list)
+        self.assertIsInstance(get_Weather_Files('2004/6', self.directoryPath), list)
+
         self.assertIsNone(get_Weather_Files('',''))
+        print('test_get_Weather_Files')
 
 
 
