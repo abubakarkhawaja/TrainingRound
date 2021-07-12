1df856480303b5ad8bf780bb102d06e6e5e6d50a
AbubakarKhawaja <abubakarkhawaja@A006-00558.local>
2021-07-12 16:55:00 +0500

    replaced use of sys.argv with argparse

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import sys
+import argparse
 
 from sourcecode.weatherByYear.weatherman import weather_by_year
 from sourcecode.weatherByYearMonth.weatherman import weather_by_year_month
 from sourcecode.weatherByYearMonthBar.weatherman import weather_by_year_month_bar
 from sourcecode.bonus.weatherman import weather_by_year_month_bar_bonus 
 
-def main():
+def main(args):
     """
     Uses options given by command line argument.
     And calls their respective function.
     """
-    arguments = sys.argv
-    pathToDirectory = arguments[1]
-
-    if "-e" in arguments:
-        print('\nTask 1:')
-        weather_by_year(pathToDirectory, date=arguments[arguments.index('-e')+1])
+    pathToDirectory = args.pathToDirectory
+    if args.e:
+        print('\nWeather Data by Year:')
+        weather_by_year(pathToDirectory, date=args.e)
         print('\n')
-    if "-a" in arguments:
-        print('\nTask 2:')
-        weather_by_year_month(pathToDirectory, date=arguments[arguments.index('-a')+1])
+    if args.a:
+        print('\nWeather Data by Year & Month:')
+        weather_by_year_month(pathToDirectory, date=args.a)
         print('\n')
-    if "-c" in arguments:
-        print('\nTask 3:')
-        weather_by_year_month_bar(pathToDirectory, date=arguments[arguments.index('-c')+1])
-    if "-d" in arguments:
-        print('\nTask 5:')
-        weather_by_year_month_bar_bonus(pathToDirectory, date=arguments[arguments.index('-d')+1])
+    if args.c:
+        print('\nWeather Data by Year & Month with Coloured Bars')
+        weather_by_year_month_bar(pathToDirectory, date=args.c)
+    if args.d:
+        print('\nWeather Data by Year & Month with Coloured Bars (Bonus)')
+        weather_by_year_month_bar_bonus(pathToDirectory, date=args.d)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pathToDirectory", help="Path to directory")
+    parser.add_argument("-e", help="Weather Data by Year. i.e. -e 2011")
+    parser.add_argument("-a", help="Weather Data by Month & Year. i.e. -a 2011/8")
+    parser.add_argument("-c", help="Weather Data by Month & Year with Bar. i.e. -c 2004/8")
+    parser.add_argument("-d", help="Weather Data by Month & Year with Bar Bonus. i.e. -d 2004/8")
+    args = parser.parse_args()
+    main(args)
