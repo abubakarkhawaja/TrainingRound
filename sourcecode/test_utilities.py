7eadbbc63de0c8bb39c16ad0c2067b6c3195d579
AbubakarKhawaja <abubakarkhawaja@A006-00558.local>
2021-07-12 17:41:08 +0500

    removed useless function and print statements in test_utilies

--- sourcecode/test_utilities.py
+++ sourcecode/test_utilities.py
@@ -3,34 +3,15 @@ from utilities import get_weather_info, get_weather_files
 
 class TestUtilites(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        """Will run at start of unittesting this class"""
-        print('setUpClass')
-    
-    @classmethod
-    def tearDownClass(cls) -> None:
-        """Will run at end of unittesting this class"""
-        print('\ntearDownClass')
+    def test_get_weather_info(self):
+        filePath = '/Users/abubakarkhawaja/Documents/weatherfiles/Murree_weather_2004_Aug.txt'
 
-    def setUp(self):
-        """Will run before every testcase"""
-        print('\nsetUp')
-        self.filePath = '/Users/abubakarkhawaja/Documents/weatherfiles/Murree_weather_2004_Aug.txt'
-        self.directoryPath = '/Users/abubakarkhawaja/Documents/weatherfiles/'
-        
-    def tearDown(self) -> None:
-        """Will run after every testcase"""
-        print('tearDown\n')
-
-    def test_get_weather_info(self) -> None:
-        self.assertIsInstance(get_weather_info(self.filePath), dict)    
+        self.assertIsInstance(get_weather_info(filePath), dict)    
         
         self.assertRaises(FileNotFoundError,get_weather_info,'')
-        print('test_get_weather_info')
 
-    def test_get_weather_files(self) -> None:
-        self.assertIsInstance(get_weather_files('2004/6', self.directoryPath), list)
+    def test_get_weather_files(self):
+        directoryPath = '/Users/abubakarkhawaja/Documents/weatherfiles/'
+        self.assertIsInstance(get_weather_files('2004/6', directoryPath), list)
 
         self.assertIsNone(get_weather_files('',''))
-        print('test_get_weather_files')
