4acbddbc0dd07db756f8bcfaa78ec094690fd5ce
AbubakarKhawaja <abubakarkhawaja@A006-00558.local>
2021-07-13 15:11:43 +0500

    unittest updated to weatherman_service

--- sourcecode/test_utilities.py
+++ sourcecode/test_utilities.py
@@ -1,17 +1,20 @@
 import unittest
-from utilities import get_weather_info, get_weather_files
+from sourcecode.weather_services import WeatherMan
+
 
 
 class TestUtilites(unittest.TestCase):
 
     path = '/Users/abubakarkhawaja/Documents/weatherfiles/'
+    obj = WeatherMan()
 
     def test_get_weather_info(self):
         file_path = self.path + 'Murree_weather_2004_Aug.txt'
         
-        self.assertIsInstance(get_weather_info(file_path), list)    
-        self.assertRaises(FileNotFoundError,get_weather_info,'')
+        self.assertIsInstance(self.obj.get_weather_info(file_path), list)    
+        self.assertRaises(FileNotFoundError, self.get_weather_info, '')
 
     def test_get_weather_files(self):
-        self.assertIsInstance(get_weather_files('2004/6', self.path), list)
-        self.assertIsNone(get_weather_files('',''))
+        self.assertIsInstance(self.get_weather_files('2004/6', self.path), 
+                            list)
+        self.assertIsNone(self.get_weather_files('', ''))
