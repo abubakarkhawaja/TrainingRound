4b674e7b568474588477744e4ec46a27bcc78c0e
AbubakarKhawaja <abubakarkhawaja@A006-00536.local>
2021-08-02 10:50:01 +0500

    pr fix

--- jacklemkus/jacklemkus/spiders/jacklemkus_spider.py
+++ jacklemkus/jacklemkus/spiders/jacklemkus_spider.py
@@ -1,45 +1,26 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-
-class ItemSpider(CrawlSpider):
-    name = 'jacklemkus'
-    allowed_domains = ['jacklemkus.com']
-    start_urls = ['https://www.jacklemkus.com']
-
-    rules = [
-        Rule(LinkExtractor(allow=r'^https://www.jacklemkus.com/(\w+$|(\w+-\w+$))', restrict_css='#nav'), callback='parse_pagination'),
-        Rule(LinkExtractor(restrict_css='#products-grid'), callback='parse')
-    ]
-
-    def parse_pagination(self, response):
-        last_page = response.css("div.js-infinite-scroll-pager-data::attr(data-lastpage)").get()
-        if last_page:
-            for page_number in range(1, int(last_page)+1):
-                url = response.urljoin(f"?p={page_number}")
-                yield response.follow(url, callback=self._parse)
-
+class ProductPageParser:
     def parse(self, response) -> dict:
-        product_parser = ProductPageParser()
         product = {}
         product['retailer'] = 'jacklemkus'
-        product['spider_name'] = "jacklemkus"
-        product['retailer-sku'] = product_parser.retailer_sku(response)
-        product['name'] = product_parser.product_name(response)
-        product['gender'] = product_parser.gender(product_parser.raw_description(response))
+        product['spider_name'] = 'jacklemkus'
+        product['retailer-sku'] = self.retailer_sku(response)
+        product['name'] = self.product_name(response)
+        product['gender'] = self.gender(self.raw_description(response))
         product['url'] = response.url
-        product['decription'] = product_parser.raw_description(response)
+        product['decription'] = self.raw_description(response)
         product['market'] = 'ZA'
-        product['skus'] = product_parser.skus_content(response)
-        product['price'] = product_parser.product_price(response)
-        product['catagory'] = [product_parser.product_brand(product_parser.raw_description(response))]
-        product['image_urls'] = product_parser.image_urls(response)
-        product['brand'] = product_parser.product_brand(product_parser.raw_description(response))
+        product['skus'] = self.skus_content(response)
+        product['price'] = self.product_price(response)
+        product['catagory'] = [self.product_brand(self.raw_description(response))]
+        product['image_urls'] = self.image_urls(response)
+        product['brand'] = self.product_brand(self.raw_description(response))
         product['currency'] = 'ZAR'
         product['environment'] = 'production'
         yield product
 
-class ProductPageParser:
     def retailer_sku(self, response):
         return response.css('.sku::text').get()
 
@@ -51,30 +32,32 @@ class ProductPageParser:
 
     def product_brand(self, raw_description: list) -> str:
         product_brand = 'n/a'
-        if 'Item Brand' in raw_description:
-            brand_index = raw_description.index('Item Brand') + 1
-            product_brand = raw_description[brand_index]
+        if 'Item Brand' not in raw_description:
+            return product_brand
+        brand_index = raw_description.index('Item Brand') + 1
+        product_brand = raw_description[brand_index]
         return product_brand
 
     def gender(self, raw_description: list) -> str:
         gender = 'unisex'
-        if 'Gender' in raw_description:
-            gender_index = raw_description.index('Gender') + 1
-            gender = raw_description[gender_index]
-            gender = gender.split(' ')[0].lower()
-            if gender.endswith('s'):
-                gender = gender[:-1]
+        if 'Gender' not in raw_description:
+            return gender
+        gender_index = raw_description.index('Gender') + 1
+        gender = raw_description[gender_index]
+        gender = gender.split(' ')[0].lower()
+        if gender.endswith('s'):
+            gender = gender[:-1]
         return gender
 
     def skus_content(self, response) -> list[dict]:
         skus = []
         for sku in  response.css('.list-size li '):            
             sku_content = {}
-            sku_content["currency"] = "ZAR"
-            sku_content["out_of_stock"] = False if  response.css('#product_addtocart_form') else True
-            sku_content["price"] = self.product_price(response)
-            sku_content["sku_id"] = int(sku.css('button::attr(data-productid)').get())
-            sku_content["size"] = sku.css('button::text').get().replace(" ","").strip('\n')
+            sku_content['currency'] = 'ZAR'
+            sku_content['out_of_stock'] = False if  response.css('#product_addtocart_form') else True
+            sku_content['price'] = self.product_price(response)
+            sku_content['sku_id'] = int(sku.css('button::attr(data-productid)').get())
+            sku_content['size'] = sku.css('button::text').get().replace(" ","").strip('\n')
             skus.append(sku_content)
         return sku_content
 
@@ -100,3 +83,24 @@ class ProductPageParser:
     def product_price(self, response) -> float:
         price = response.css('.price::text').re_first(r'R\s*(.*)')
         return float(price.replace(',', ''))
+
+class ItemSpider(ProductPageParser, CrawlSpider):
+    name = 'jacklemkus'
+    allowed_domains = ['jacklemkus.com']
+    start_urls = ['https://www.jacklemkus.com']
+    listing_css = '#nav'
+    products_css = '#products-grid'
+    single_depth_url = r'^https://www.jacklemkus.com/(\w+$|(\w+-\w+$))'
+
+    rules = [
+        Rule(LinkExtractor(allow=single_depth_url, restrict_css=listing_css), callback='parse_pagination'),
+        Rule(LinkExtractor(restrict_css=products_css), callback='parse')
+    ]
+
+    def parse_pagination(self, response):
+        last_page = response.css('div.js-infinite-scroll-pager-data::attr(data-lastpage)').get()
+        if not last_page:
+            return None
+        for page_number in range(1, int(last_page)+1):
+            url = response.urljoin(f'?p={page_number}')
+            yield response.follow(url, callback=self._parse)
