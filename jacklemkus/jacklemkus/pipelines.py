222c8ab5b3755544f4aadbf8bd2de5a50604df6a
AbubakarKhawaja <abubakarkhawaja@A006-00614.local>
2021-08-03 16:29:30 +0500

    pr fix

--- jacklemkus/jacklemkus/pipelines.py
+++ jacklemkus/jacklemkus/pipelines.py
@@ -10,4 +10,19 @@ from itemadapter import ItemAdapter
 
 class JacklemkusPipeline:
     def process_item(self, item, spider):
+        item['retailer'] = 'jacklemkus'
+        item['spider_name'] = 'jacklemkus'
+        item['retailer-sku'] = spider.retailer_sku()
+        item['name'] = spider.item_name()
+        item['gender'] = spider.gender()
+        item['url'] = spider.url()
+        item['decription'] = spider.raw_description()
+        item['market'] = 'ZA'
+        item['skus'] = spider.skus_content()
+        item['brand'] = spider.product_brand()
+        item['price'] = spider.product_price()
+        item['image_urls'] = spider.image_urls()
+        item['catagory'] = [item['brand']]
+        item['currency'] = item['skus'][0].get('currency')
+        item['environment'] = 'production'
         return item
--- jacklemkus/jacklemkus/spiders/jacklemkus_spider.py
+++ jacklemkus/jacklemkus/spiders/jacklemkus_spider.py
@@ -4,67 +4,56 @@ from scrapy.linkextractors import LinkExtractor
 import w3lib.url
 
 class ProductPageParser:
+    GENDERS = ['Mens', 'Womens', 'Kids']
+
     def parse(self, response) -> dict:
-        product = {}
-        product['retailer'] = 'jacklemkus'
-        product['spider_name'] = 'jacklemkus'
-        product['retailer-sku'] = self.retailer_sku(response)
-        product['name'] = self.product_name(response)
-        product['gender'] = self.gender(response)
-        product['url'] = response.url
-        product['decription'] = self.raw_description(response)
-        product['market'] = 'ZA'
-        product['skus'] = self.skus_content(response)
-        product['price'] = self.product_price(response)
-        product['catagory'] = [self.product_brand(response)]
-        product['image_urls'] = self.image_urls(response)
-        product['brand'] = self.product_brand(response)
-        product['currency'] = 'ZAR'
-        product['environment'] = 'production'
-        yield product
+        ProductPageParser.response = response
+        yield {}
 
-    def retailer_sku(self, response):
-        return response.css('.sku::text').get()
+    def retailer_sku(self):
+        return ProductPageParser.response.css('.sku::text').get()
 
-    def product_name(self, response):
-        return response.css('.product-name h1::text').get()
+    def product_name(self):
+        return ProductPageParser.response.css('.product-name h1::text').get()
 
-    def image_urls(self, response) -> list:
-        return response.css('.span1 div a img::attr(src)').getall()
+    def url(self):
+        return ProductPageParser.response.url
 
-    def product_brand(self, response) -> str:
-        raw_description = self.raw_description(response)
+    def image_urls(self) -> list:
+        return ProductPageParser.response.css('.span1 div a img::attr(src)').getall()
+
+    def product_brand(self) -> str:
+        raw_description = self.raw_description()
         product_brand = 'n/a'
         if 'Item Brand' not in raw_description:
             return product_brand
         brand_index = raw_description.index('Item Brand') + 1
         return raw_description[brand_index]
 
-    def gender(self, response) -> str:
-        description = self.raw_description(response)
-        genders = ['Mens', 'Womens', 'Kids']
-        for gender in genders:
+    def gender(self) -> str:
+        description = self.raw_description()
+        for gender in self.GENDERS:
             if gender in description:
                 return gender.lower().rstrip('s')
         return 'unisex'
 
-    def skus_content(self, response) -> list[dict]:
+    def skus_content(self) -> list[dict]:
         skus = []
-        for sku in  response.css('.list-size li '):            
+        for sku_s in  ProductPageParser.response.css('.list-size li '):            
             sku_content = {}
             sku_content['currency'] = 'ZAR'
-            sku_content['out_of_stock'] = False if  response.css('#product_addtocart_form') else True
-            sku_content['price'] = self.product_price(response)
-            sku_content['sku_id'] = int(sku.css('button::attr(data-productid)').get())
-            sku_content['size'] = sku.css('button::text').get().replace(" ","").strip('\n')
+            sku_content['out_of_stock'] = False if  ProductPageParser.response.css('#product_addtocart_form') else True
+            sku_content['price'] = self.product_price()
+            sku_content['sku_id'] = int(sku_s.css('button::attr(data-productid)').get())
+            sku_content['size'] = sku_s.css('button::text').get().replace(" ","").strip('\n')
             skus.append(sku_content)
         return sku_content
 
-    def raw_description(self, response) -> list:
-        row = response.xpath('//*[@id="product-attribute-specs-table"]/tbody/tr')
+    def raw_description(self) -> list:
+        row = ProductPageParser.response.css('#product-attribute-specs-table > tbody > tr')
         column_lable = [cell.css('th::text').get() for cell in row]
         column_data = [cell.css('td::text').get() for cell in row]
-        brand_description = response.css('.std::text').get()
+        brand_description = ProductPageParser.response.css('.std::text').get()
         
         column_lable_and_data = self.append_column_lable_and_data(column_lable, column_data)
         column_lable_and_data.insert(0, brand_description)
@@ -79,23 +68,24 @@ class ProductPageParser:
             description.append(column_data[i])
         return description
 
-    def product_price(self, response) -> float:
-        price = response.css('.price::text').re_first(r'R\s*(.*)')
+    def product_price(self) -> float:
+        price = ProductPageParser.response.css('.price::text').re_first(r'R\s*(.*)')
         return float(price.replace(',', ''))
 
 class ItemSpider(ProductPageParser, CrawlSpider):
     name = 'jacklemkus'
     allowed_domains = ['jacklemkus.com']
     start_urls = ['https://www.jacklemkus.com']
-    restricted_css = ['#nav > li.level0 > a.menu-link', '#products-grid']
+    listing_css = ['#nav > li.level0 > a.menu-link']
+    products_css = ['#products-grid']
 
     rules = [
-        Rule(LinkExtractor(restrict_css=restricted_css[0]), callback='parse_pagination'),
-        Rule(LinkExtractor(restrict_css=restricted_css[1]), callback='parse')
+        Rule(LinkExtractor(restrict_css=listing_css[0]), callback='parse_pagination'),
+        Rule(LinkExtractor(restrict_css=products_css[0]), callback='parse')
     ]
 
     def parse_pagination(self, response):
-        last_page = response.css('div.js-infinite-scroll-pager-data::attr(data-lastpage)').get()
+        last_page = self.response.css('div.js-infinite-scroll-pager-data::attr(data-lastpage)').get()
         
         if not last_page:
             return None
