cc95e12b6f67ea825618f05d5018444956a8c7e4
AbubakarKhawaja <abubakarkhawaja@A006-00614.local>
2021-08-06 19:00:50 +0500

    Database attached

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+
+class Description(models.Model):
+    retailer_sku = models.ForeignKey('Products', models.DO_NOTHING, db_column='retailer_sku', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'description'
+
+class ImageUrls(models.Model):
+    retailer_sku = models.ForeignKey('Products', models.DO_NOTHING, db_column='retailer_sku', blank=True, null=True)
+    image_url = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'image_urls'
+
+
+class Products(models.Model):
+    retailer = models.TextField(blank=True, null=True)
+    spider_name = models.TextField(blank=True, null=True)
+    retailer_sku = models.IntegerField(primary_key=True)
+    name = models.TextField(blank=True, null=True)
+    gender = models.TextField(blank=True, null=True)
+    url = models.TextField(blank=True, null=True)
+    market = models.TextField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    catergory = models.TextField(blank=True, null=True)
+    brand = models.TextField(blank=True, null=True)
+    currency = models.TextField(blank=True, null=True)
+    environment = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'products'
+
+
+class Skus(models.Model):
+    retailer_sku = models.ForeignKey(Products, models.DO_NOTHING, db_column='retailer_sku', blank=True, null=True)
+    currency = models.TextField(blank=True, null=True)
+    out_of_stock = models.IntegerField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    sku_id = models.IntegerField(blank=True, null=True)
+    size = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'skus'
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Products
 
-# Create your views here.
+def index(request):
+    products = Products.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'store/index.html', context)
